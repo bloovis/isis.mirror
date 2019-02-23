49861942b41938ec8a2ce3b758dd51470c649500
marka@pobox.com <marka@pobox.com>
2019-02-23 19:12:12 +0000

    Add copyright and GPL notice to the currently used source files.
    FossilOrigin-Name: 2711ddac3ed867a131aecbd9f952c65361b941806f296ef0985f4ef691574f6a

--- isis.c
+++ isis.c
@@ -1,4 +1,24 @@
-/*  isis.c - ISIS-II simulator for IBM PC */
+/*
+    isis.c - ISIS-II simulator for IBM PC
+
+    Copyright (C) 2019 Mark Alexander
+
+    This file is part of Isis, an ISIS-II simulator
+
+    Isis is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
